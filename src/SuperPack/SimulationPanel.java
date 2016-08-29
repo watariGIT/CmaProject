0f8c047c273192bce44f31fea7ee7ebc1e623f98
watari <6316605@ed.tus.ac.jp>
2016-08-29 17:22:46 +0900

    削除マップの追加

--- src/SuperPack/SimulationPanel.java
+++ src/SuperPack/SimulationPanel.java
@@ -12,7 +12,7 @@ abstract public class SimulationPanel extends JPanel {
     public final static int length = 250;
     public final static int size = 1000;
     public static int robotsNum = 20;            //ロボットの初期台数
-    public static int targetNum = 10;            //ターゲットの初期台数
+    public static int targetNum = 5;            //ターゲットの初期台数
     private final static int maxCount = 10000;//最大の捕獲時間
     public Robot[] robot;
     public ArrayList<Enemy> targetList = new ArrayList<Enemy>();
