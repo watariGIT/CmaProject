9392d028305f767a820dc763fa062611edd96e3c
watariGIT <6316605@ed.tus.ac.jp>
2016-11-10 16:14:58 +0900

    TODOの整理

--- src/GsoPanel/GsoRobot.java
+++ src/GsoPanel/GsoRobot.java
@@ -10,16 +10,47 @@ import java.awt.*;
  * Created by watariMac on 2016/11/10.
  */
 public class GsoRobot extends Robot {
+    //TODO 各パラメータをメンバに追加
+
+
+
     protected GsoRobot(SimulationPanel s) {
+        //TODO パラメータの初期化
         super(s);
     }
 
     protected GsoRobot(Point2 point, SimulationPanel s) {
+        //TODO パラメータの初期化
         super(point, s);
+
     }
 
     protected GsoRobot(SimulationPanel s, String robotString) {
         super(s, robotString);
+        //TODO パラメータの初期化
+
+    }
+
+    @Override
+    public void move(){
+
+    }
+
+
+
+    @Override
+    protected  void captured(){
+        //TODO 捕獲した時の処理
+    }
+
+    @Override
+    public void copy(Robot robot){
+        //TODO
+    }
+
+    @Override
+    public  void  reset(){
+        //TODO
     }
 
     @Override
@@ -29,6 +60,8 @@ public class GsoRobot extends Robot {
 
     @Override
     public void setCI() {
-
+        //TODO うまく
     }
+
+    //TODO ハッショコードとequalメソッド
 }
--- src/GsoPanel/GsoSimulation.java
+++ src/GsoPanel/GsoSimulation.java
@@ -1,27 +1,60 @@
 package GsoPanel;
 
+import SuperPack.Panel.Enemy;
 import SuperPack.Panel.SimulationPanel;
+
+import java.util.ArrayList;
 
 /**
  * Created by watariMac on 2016/11/10.
  */
 public class GsoSimulation extends SimulationPanel{
+
+
+    /**
+     * コンストラクタ
+     */
+    public GsoSimulation() {
+        robot = new GsoRobot[robotsNum];
+
+        for (int i = 0; i < targetNum; i++)
+            targetList.add(new Enemy());
+        for (int i = 0; i < robotsNum; i++)
+            robot[i] = new GsoRobot(this);
+    }
+
     @Override
     public void readRobotFile(String str) {
+        //todo
     }
 
     @Override
     public void reset() {
+        count = 0;
+        communication_num = 0;
+        targetList.clear();
+        for (int i = 0; i < targetNum; i++)
+            targetList.add(new Enemy());
+
+        for (int i = 0; i < robotsNum; i++)
+            robot[i].reset();
+        for (int i = 0; i < robotsNum; i++)
+            robot[i].setCI();
 
     }
 
     @Override
     public void step() {
-
+        count++;
+        for (int i = 0; i < robotsNum; i++) {
+            robot[i].move();
+        }
+        //ターゲットの削除
+        deleteCaptureTarget();
     }
 
     @Override
     public void readFile(String fileString) {
-
+        //todo
     }
 }
