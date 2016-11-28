6a10adb5e8d6900508285ab214932ee009d6b256
watariGIT <6316605@ed.tus.ac.jp>
2016-11-28 20:48:21 +0900

    AFS用のABCによるパラメータ推定プログラムの実装

--- src/OptimizeParam/optimizeMain.java
+++ src/OptimizeParam/optimizeMain.java
@@ -14,12 +14,12 @@ public class OptimizeMain {
     public static void main(String args[]) {
         ArrayList<Particle> particles = new ArrayList<>();
         Particle globalBest;
-        int experiencedNum=40;
-        int onlookerNum=20;
-        int scoutNum=40;
+        int experiencedNum = 20;
+        int onlookerNum = 10;
+        int scoutNum = 10;
 
 
-        for (int i = 0; i < experiencedNum+onlookerNum+scoutNum; i++) {
+        for (int i = 0; i < experiencedNum + onlookerNum + scoutNum; i++) {
             particles.add(new Particle(4));
             if (i % 10 != 0) {
                 System.out.printf(" ");
@@ -30,19 +30,22 @@ public class OptimizeMain {
         System.out.println("|");
         calsFitness(particles);
 
-       Collections.sort(particles);
+        Collections.sort(particles);
         globalBest = new Particle(particles.get(0));
         System.out.println(globalBest.toString());
+        for (Particle p : particles)
+            System.out.println(p);
 
-        for (int count = 0; count < 10; count++) {
-            //GSOに従った位置の決定
+
+        for (int count = 0; count < 100; count++) {
+            //ABCに従った位置の決定
             Particle eBee = particles.get((int) (Math.random() * 40.0));
             for (int i = 0; i < particles.size(); i++) {
                 Particle p = particles.get(i);
                 if (i < experiencedNum) {
                     //評価値の高いやつ
                     p.experiencedBee(globalBest);
-                } else if (i < experiencedNum+onlookerNum) {
+                } else if (i < experiencedNum + onlookerNum) {
                     //中位
                     p.onlookerBee(eBee);
                 } else {
@@ -56,20 +59,25 @@ public class OptimizeMain {
             if (particles.get(0).fittness < globalBest.fittness) {
                 globalBest = new Particle(particles.get(0));
             }
+
             System.out.println("count :" + count);
             System.out.println(globalBest.toString());
+            if (count % 10 == 0) {
+                for (Particle p : particles)
+                    System.out.println(p);
+            }
         }
 
     }
 
     private static void calsFitness(ArrayList<Particle> particles) {
         for (Particle p : particles) {
-            double sum=0;
-            for(int i=0;i<10;i++) {
+            double sum = 0;
+            for (int i = 0; i < 10; i++) {
                 AfsSimulation As = new AfsSimulation(100, p.xlist.get(0), p.xlist.get(1), p.xlist.get(2), p.xlist.get(3));
-                sum+=As.getCapturedStep();
+                sum += As.getCapturedStep();
             }
-            p.setFitness(sum/10);
+            p.setFitness(sum / 10);
             System.out.print(":");
         }
         System.out.println("END");
